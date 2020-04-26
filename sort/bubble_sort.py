869d55369ef771c777a5c91dca526ceef6469e78
Shaon <shaon63@gmail.com>
2020-04-26 23:59:33 +0600

    first commit

---
+++ sort/bubble_sort.py
@@ -0,0 +1,24 @@
+"""
+Python 3.8.2
+Pure Python Implementation of Bubble sort algorithm
+Complexity is O(n^2)
+"""
+
+
+def bubble_sort(arr):
+    """Sorting array with bubble sort algorithm"""
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j] # Swaping here
+
+    return arr
+
+
+if __name__ == '__main__':
+
+    # Taking input from user
+    USER_INPUT = [int(x) for x in input().split()]
+
+    # call bublesort to sort an unsorted list.
+    print(bubble_sort(USER_INPUT))
---
+++ sort/selection_sort.py
@@ -0,0 +1,28 @@
+"""
+Python version: 3.8.2
+This is pure python implementation of selection sort.
+Complexity is O(n^2)
+"""
+
+
+def selection_sort(collection):
+    """Sort a list by selection sort"""
+    for i in enumerate(collection): #Outer loop
+        mn_pos = i[0]
+        for j in range(i[0], len(collection)): #Inner loop
+            if collection[j] < collection[mn_pos]:
+                mn_pos = j
+
+        # Swaping here
+        collection[i[0]], collection[mn_pos] = collection[mn_pos], collection[i[0]]
+
+    return collection
+
+
+if __name__ == "__main__":
+
+    # Taking input from user to sort.
+    USER_INPUT = [int(x) for x in input().split()]
+
+    # Call selection sort to sort arr list.
+    print(selection_sort(USER_INPUT))
